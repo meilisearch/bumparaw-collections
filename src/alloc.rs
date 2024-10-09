0ecd143c1707d237e3c4d749bc685418da2fccc2
Louis Dureuil <louis@meilisearch.com>
2024-10-09 11:07:04 +0200

    Add more operations and test them

--- src/alloc.rs
+++ src/alloc.rs
@@ -14,11 +14,48 @@ impl<'bump> RefBump<'bump> {
     pub fn new(r: Ref<'bump, Bump>) -> Self {
         Self(r)
     }
-}
 
-impl<'bump> Clone for RefBump<'bump> {
-    fn clone(&self) -> Self {
-        Self(Ref::clone(&self.0))
+    /// Clones the wrapped `Ref<Bump>`.
+    ///
+    /// See [`Ref::clone`] for more information.
+    #[allow(clippy::should_implement_trait)] // really, go see [`Ref::clone`]
+    pub fn clone(orig: &RefBump<'bump>) -> RefBump<'bump> {
+        Self(Ref::clone(&orig.0))
+    }
+
+    /// Makes a new [`Ref`] for a component of the borrowed data.
+    ///
+    /// See [`Ref::map`] for more information.
+    pub fn map<T, F>(orig: RefBump<'bump>, f: F) -> Ref<'bump, T>
+    where
+        F: FnOnce(&Bump) -> &T,
+        T: ?Sized,
+    {
+        Ref::map(orig.0, f)
+    }
+
+    /// Makes a new [`Ref`] for an optional component of the borrowed data.
+    /// The original guard is returned as an `Err(..)` if the closure returns None.
+    ///
+    /// See [`Ref::filter_map`] for more information.
+    pub fn filter_map<T, F>(orig: RefBump<'bump>, f: F) -> Result<Ref<'bump, T>, RefBump<'bump>>
+    where
+        F: FnOnce(&Bump) -> Option<&T>,
+        T: ?Sized,
+    {
+        Ref::filter_map(orig.0, f).map_err(RefBump)
+    }
+
+    /// Splits a Ref into multiple Refs for different components of the borrowed data.
+    ///
+    /// See [`Ref::filter_map`] for more information.
+    pub fn map_split<T, U, F>(orig: RefBump<'bump>, f: F) -> (Ref<'bump, T>, Ref<'bump, U>)
+    where
+        F: FnOnce(&Bump) -> (&T, &U),
+        T: ?Sized,
+        U: ?Sized,
+    {
+        Ref::map_split(orig.0, f)
     }
 }
 
--- src/test.rs
+++ src/test.rs
@@ -1,8 +1,11 @@
-use std::cell::RefCell;
+use std::{
+    cell::{Ref, RefCell},
+    ops::Deref as _,
+};
 
 use bumpalo::Bump;
 
-use crate::RawMap;
+use crate::{alloc::RefBump, RawMap};
 
 #[test]
 fn it_works() {
@@ -27,11 +30,11 @@ fn with_alloc() {
 #[test]
 fn test_ref_allocator() {
     let bump = RefCell::new(Bump::new());
-    let bump_ref = crate::alloc::RefBump::new(bump.borrow());
+    let bump_ref = RefBump::new(bump.borrow());
 
     let hash = {
         let s = bump_ref.alloc_str("titi");
-        let mut hash = hashbrown::HashMap::new_in(bump_ref.clone());
+        let mut hash = hashbrown::HashMap::new_in(RefBump::clone(&bump_ref));
         hash.insert(s, 42);
 
         hash
@@ -41,5 +44,43 @@ fn test_ref_allocator() {
 
     drop(hash);
     drop(bump_ref);
+    assert!(bump.try_borrow_mut().is_ok());
+}
+
+#[test]
+fn test_ref_map() {
+    struct RefStr<'bump>(pub Ref<'bump, str>);
+
+    impl<'bump> std::hash::Hash for RefStr<'bump> {
+        fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+            self.0.hash(state);
+        }
+    }
+
+    impl<'bump> PartialEq for RefStr<'bump> {
+        fn eq(&self, other: &Self) -> bool {
+            self.0.deref() == other.0.deref()
+        }
+    }
+
+    impl<'bump> Eq for RefStr<'bump> {}
+
+    let bump = RefCell::new(Bump::new());
+    let bump_ref = RefBump::new(bump.borrow());
+
+    let hash = {
+        let mut hash = hashbrown::HashMap::new_in(bump_ref);
+        let bump_ref_clone = RefBump::clone(hash.allocator());
+        let s = RefStr(RefBump::map(bump_ref_clone, |bump| bump.alloc_str("toto")));
+
+        hash.insert(s, 42);
+
+        hash
+    };
+
+    assert!(bump.try_borrow_mut().is_err());
+
+    drop(hash);
+
     assert!(bump.try_borrow_mut().is_ok());
 }
