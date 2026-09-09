ee866707d76bec613cd23134f56e70d28a977c95
Louis Dureuil <louis@meilisearch.com>
2026-09-09 10:26:41 +0200

    Update src/frozen.rs
    Co-authored-by: Clément Renault <clement@meilisearch.com>

--- src/frozen.rs
+++ src/frozen.rs
@@ -4,7 +4,7 @@
 ///
 /// # Safety
 ///
-/// TBD
+/// It is only safe to implement on types that are not referencing any thread local values as the type will be `Send` and can be used on different threads.
 pub unsafe trait Freezable<'a> {
     /// Result of frozing the object.
     type Frozen: 'a + Send;
